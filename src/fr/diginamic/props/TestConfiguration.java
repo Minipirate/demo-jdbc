8903ca945f86bdc1a3921e7d9dee29d2bed44a2b
git <nessti@live.fr>
2021-02-19 13:51:42 +0100

    TP1 -> Connexion

--- src/fr/diginamic/props/TestConfiguration.java
+++ src/fr/diginamic/props/TestConfiguration.java
@@ -11,6 +11,7 @@ import java.util.ResourceBundle;
  *
  */
 public class TestConfiguration {
+	
 
 	/**
 	 * @param args
---
+++ src/fr/diginamic/props/TestConnectionJdbc.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package fr.diginamic.props;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.mariadb.jdbc.Driver;
+
+/**
+ * @author Ness'ti
+ *
+ */
+public class TestConnectionJdbc {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		try {
+			// 1 => chargement du driver (classe fournie par la lib Mariadb
+			DriverManager.registerDriver(new Driver());
+
+		} catch (SQLException e) {
+			System.out.println("Impossible de charger le driver MariaDB !");
+		}
+		try {
+			// 2 => demander au DriverManager de fournir une co à une bdd de type MariaDB
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/formation", "root", "");
+
+			// 3 => Fermeture de la co
+			System.out.println(connection.isClosed());
+
+		} catch (SQLException e) {
+			System.out.println("Impossible de se connecter à la base de données" + e.getMessage());
+		}
+	}
+}
